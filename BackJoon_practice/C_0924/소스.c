c25d4e045751b27ec341b7c2777292818d2d94db
Hyeong1204 <cho4870@naver.com>
2022-09-24 14:42:31 +0900

    09.24 - 배열2

--- BackJoon_practice/C_0924/소스.c
+++ BackJoon_practice/C_0924/소스.c
@@ -1,13 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
-	int array[101];
+	
+	// 데이터를 순차적으로 저장해야 할 때가 있습니다.
+	// 우리는 배열을 사용합니다.
 
-	for (int i = 0; i < 101; i++) {
-		array[i] = i;
+	// DataType 배열명[배열의 개수];
+
+	float heightArray[10];
+
+	for (int i = 0; i < 10; i++) {
+		heightArray[i] = 1.2f * i;
 	}
 
-	for (int i = 0; i < 101; i++) {
-		printf("array[%d] = %d\n", i, array[i]);
+	for (int i = 0; i < 10; i++) {
+		if (heightArray[i] == 3.6f) {
+			printf("3.6f는 배열의 %d 번째 들어있습니다. \n", i);
+		}
 	}
+
+	float sum = 0.0f;
+	for (int i = 0; i < 10; i++) {
+		sum += heightArray[i];
+	}
+
+	printf("배열안에 들은 숫자의 합계는 : %f입니다.\n", sum);
+
+	return 0;
 }
