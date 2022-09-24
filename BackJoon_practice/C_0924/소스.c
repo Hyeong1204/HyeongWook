965455f55b07e3d41a44dd7c389c347fe1b8212f
Hyeong1204 <cho4870@naver.com>
2022-09-24 15:58:00 +0900

    09.24 - 구조체 와 포인터

--- BackJoon_practice/C_0924/소스.c
+++ BackJoon_practice/C_0924/소스.c
@@ -1,29 +1,20 @@
 #include <stdio.h>
 
-struct tagStudent {		// 구조체 (사용자 정의 데이터 타입)
-	int No;		// 학번	(구조체 멤버)
-	int Age;	// 나이
-	int Grade;	// 학년
-	int ClassNum;	// 반번호
-	
-};
-
-typedef struct tagStudent Student;
 
 int main(void) {
 	
-	Student array[10];	// 구조체 배열
+	int array[10];
+
+	array[0] = 10;		// 배열식
+
+	*(array + 1) = 20;		// 포인터 식
 
 	for (int i = 0; i < 10; i++) {
-		array[i].No = i;
-		array[i].Age = i + 8;
-		array[i].Grade = i % 3 + 1;		// 1~4 나옴
+		*(array + i) = i;
 	}
 
 	for (int i = 0; i < 10; i++) {
-		printf("array[%d].NO = %d\n", i, array[i].No);
-		printf("array[%d].Age = %d\n", i, array[i].Age);
-		printf("array[%d].Grade = %d\n", i, array[i].Grade);
+		printf("*(array + %d) = %d\n", i, *(array + i));
 	}
 
 	return 0;
