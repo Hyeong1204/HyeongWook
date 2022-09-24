539e092453138c3d59b8bd3491e90328dbfb0a0e
Hyeong1204 <cho4870@naver.com>
2022-09-24 15:20:44 +0900

    09.24 - 구조체2

--- BackJoon_practice/C_0924/소스.c
+++ BackJoon_practice/C_0924/소스.c
@@ -3,35 +3,27 @@
 struct Student {		// 구조체 (사용자 정의 데이터 타입)
 	int No;		// 학번	(구조체 멤버)
 	int Age;	// 나이
-	int Grande;	// 학년
+	int Grade;	// 학년
 	int ClassNum;	// 반번호
 };
 
-
 int main(void) {
 	
-	// 학생관리용 프로그램.
-	// 학번, 이름, 나이, 성별, 학년
-
-	//int No;		// 학번
-	//int Age;	// 나이
-	//int Grande;	// 학년
-	//int ClassNum;	// 반번호
-
-	// 100명
-	// 배열을 쓴다
-
-	// 하지만 더  좋은 방법이 있다.
-	// 구조체
 	struct Student a;
+	struct Student* pa = &a;
 
-	// 구조체 변수의 멤버 접근할때는 .(period) 연산자를 사용
-	a.No = 123;
-	a.Age = 10;
-	a.Grande = 2;
-	a.ClassNum = 11;
+	// 구조체의 멤버에 접근할때 구조체 변수의 주소값으로 접근하는 경우에는
+	// ->(화살표연산자)로 접근해야 한다.
 
-	printf("a.No = %d, a.Age = %d, a.Grade = %d, a.ClassNum = %d\n", a.No, a.Age, a.Grande, a.ClassNum);
+	pa->No = 23454;
+	pa->Age = 11;
+	pa->Grade = 2;
+	pa->ClassNum = 5;
+
+	printf("pa->No = %d, pa->Age = %d, pa->Grade = %d, pa->ClassNum = %d\n"
+		, pa->No, pa->Age, pa->Grade, pa->ClassNum);
+
+
 
 
 	return 0;
