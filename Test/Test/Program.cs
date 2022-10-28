3539e6a0e449d7df3954b03e416984182feab14a
Hyeong1204 <cho4870@naver.com>
2022-10-28 10:49:59 +0900

    아이템 드랍

---
+++ Test/Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] age)
+        {
+            string a;
+
+            a = solution("dfjardstddetckdaccccdegk", 4);
+
+            string solution(string cipher, int code)
+            {
+                {
+                    // slice 조각수, n 인원수
+                    string answer = "";
+                    for (int i = 1; i < cipher.Length / code + 1; i++)
+                    {
+                        answer += cipher.Substring(i * code - 1, 1);
+                    }
+
+                    return answer;
+                }
+            }
+        }
+    }
+}
