9f4a879441c79e047387f557bff3d8285a3dc37f
Hyeong1204 <cho4870@naver.com>
2022-11-30 21:17:50 +0900

    코딩 테스트 - n진법 소수 구하기

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -11,49 +11,62 @@ namespace Test
     {
         static void Main(string[] age)
         {
-            string b = "1 1 1 1 1 Z Z Z Z 1";
-            int a = solution(b);
+            int a = solution(885733, 3);
 
-            int solution(string s)
+            int solution(int n, int k)
             {
                 int answer = 0;
-                int Num = 0;
-                List<int> templist = new List<int>();
-                int count = 0;
-                string[] word = s.Split(' ');
+                List<long> numList = new List<long>();
+                string[] number = Convert(n,k).Split('0');
 
-                for (int i = 0; i < word.Length; i++)
+                for (int i = 0; i < number.Length; i++)
                 {
-                    if(i == 0 && word[0] == "Z")
+                    long.TryParse(number[i], out long c);
+                    if(c != 1)
                     {
-                        continue;
-                    }
-                    if(i != 0 && word[i] == "Z" && word[i - 1] == "Z")
-                    {
-                        count++;
-
-                        if(answer == 0)
+                        if (c != 0)
                         {
-                            continue;
+                            numList.Add(c);
                         }
-
-                        answer -= templist[templist.Count - count - 1];
-
-                        continue;
-                    }
-                    if (int.TryParse(word[i], out Num))
-                    {
-                        answer += Num;
-                        templist.Add(Num);
-                        count = 0;
-                    }
-                    else
-                    {
-                        answer = answer - templist.Last();
                     }
                 }
 
+                for (int i = 0; i < numList.Count; i++)
+                {
+                    if (SoSu(numList[i]))
+                    {
+                        answer++;
+                    }
+                }
                 return answer;
+            }
+
+            string Convert(long n, long k, string str = "")
+            {
+                long j = n / k;                  // j에 몫저장
+
+                str = (n % k).ToString() + str; // str에 나머지를 더함
+
+                if(j >= k)                      // 더 나눌것이 있다면
+                {
+                    return Convert(j, k, str);  // 한번더
+                }
+                else
+                {
+                    return str = j.ToString() + str;    // 없다면 몫 + 지금까지 모은 str
+                }
+            }
+
+            bool SoSu(long num)
+            {
+                for (int i = 2; i <= Math.Sqrt(num); i++)            // 소수 판별
+                {
+                    if (num % i == 0)
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
     }
