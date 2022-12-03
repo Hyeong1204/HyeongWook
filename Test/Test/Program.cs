6da1c9904485fa3f743123f8789def5b05dcbf8a
Hyeong1204 <cho4870@naver.com>
2022-12-03 18:01:55 +0900

    코딩 테슽트 - 양궁
    양궁 하는중

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -11,62 +11,19 @@ namespace Test
     {
         static void Main(string[] age)
         {
-            int a = solution(885733, 3);
+            int[] b = { 0, 0, 0, 0, 0, 0, 0, 0, 3, 4, 3 };
+            int[] a = solution(10, b);
 
-            int solution(int n, int k)
+            int[] solution(int n, int[] info)
             {
-                int answer = 0;
-                List<long> numList = new List<long>();
-                string[] number = Convert(n,k).Split('0');
+                int[] answer = new int[info.Length];
 
-                for (int i = 0; i < number.Length; i++)
+                for (int i = 0; i < info.Length; i++)
                 {
-                    long.TryParse(number[i], out long c);
-                    if(c != 1)
-                    {
-                        if (c != 0)
-                        {
-                            numList.Add(c);
-                        }
-                    }
+
                 }
 
-                for (int i = 0; i < numList.Count; i++)
-                {
-                    if (SoSu(numList[i]))
-                    {
-                        answer++;
-                    }
-                }
                 return answer;
-            }
-
-            string Convert(long n, long k, string str = "")
-            {
-                long j = n / k;                  // j에 몫저장
-
-                str = (n % k).ToString() + str; // str에 나머지를 더함
-
-                if(j >= k)                      // 더 나눌것이 있다면
-                {
-                    return Convert(j, k, str);  // 한번더
-                }
-                else
-                {
-                    return str = j.ToString() + str;    // 없다면 몫 + 지금까지 모은 str
-                }
-            }
-
-            bool SoSu(long num)
-            {
-                for (int i = 2; i <= Math.Sqrt(num); i++)            // 소수 판별
-                {
-                    if (num % i == 0)
-                    {
-                        return false;
-                    }
-                }
-                return true;
             }
         }
     }
